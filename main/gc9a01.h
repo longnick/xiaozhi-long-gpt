d62e7fb364683f1a09d8b817ae069a3af6616541
longnick <longnickbl@gmail.com>
2025-09-17 21:52:01 +0700

    Update gc9a01.h

--- main/gc9a01.h
+++ main/gc9a01.h
@@ -1,1 +1,13 @@
+/*
+#pragma once
+#include <stdint.h>
+#include "esp_err.h"
 
+esp_err_t gc9a01_init(int sck, int mosi, int dc, int cs, int rst, int bl);
+void gc9a01_fill(uint16_t color);
+void gc9a01_draw_bitmap(int x1, int y1, int x2, int y2, const void *data);
+
+void draw_char6x8(int x, int y, char ch, uint16_t color);
+void draw_text6x8(int x, int y, const char *s, uint16_t color);
+uint16_t rgb565(uint8_t r,uint8_t g,uint8_t b);
+*/
