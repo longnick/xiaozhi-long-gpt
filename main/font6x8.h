bd708d73e38c80a6a5b22070de1610da825989f0
longnick <longnickbl@gmail.com>
2025-09-17 22:47:14 +0700

    Update font6x8.h

--- main/font6x8.h
+++ main/font6x8.h
@@ -1,3 +1,8 @@
 #pragma once
 #include <stdint.h>
 extern const uint8_t g_font6x8[96][6];
+ ───────────────────────────────────────────────────
+/*
+#pragma once
+#include <stdint.h>
+extern const uint8_t g_font6x8[96][6];
