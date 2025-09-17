55c586df34828d15adae666fc10d45bc87345f5d
longnick <longnickbl@gmail.com>
2025-09-17 22:22:27 +0700

    Update gc9a01.h

--- main/gc9a01.h
+++ main/gc9a01.h
@@ -1,4 +1,3 @@
-/*
 #pragma once
 #include <stdint.h>
 #include "esp_err.h"
@@ -10,4 +9,3 @@ void gc9a01_draw_bitmap(int x1, int y1, int x2, int y2, const void *data);
 void draw_char6x8(int x, int y, char ch, uint16_t color);
 void draw_text6x8(int x, int y, const char *s, uint16_t color);
 uint16_t rgb565(uint8_t r,uint8_t g,uint8_t b);
-*/
